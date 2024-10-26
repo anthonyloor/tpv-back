80af467d31a541b90749d22ebc8c1007bbb8f679
Anthony Loor <111461610+anthonyloor@users.noreply.github.com>
2024-10-26 10:06:08 +0200

    [~] Modified product_search (#13)
    Co-authored-by: BKLP22 <97519892+BKLP22@users.noreply.github.com>

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -18,6 +18,8 @@ use App\Entity\PsStockAvailable;
 use App\Entity\PsShop;
 use App\Entity\PsProductAttributeImage;
 use App\Entity\PsImage;
+use App\Entity\PsSpecificPrice;
+use App\Entity\PsProductShop;
 
 
 class ProductController extends AbstractController
@@ -40,11 +42,11 @@ class ProductController extends AbstractController
         combinacion.id_product_attribute,
         stock.id_stock_available,
         tienda.id_shop,
-        nombre_producto.name as product_name,
+        nombre_producto.name AS product_name,
         GROUP_CONCAT(DISTINCT nombre_combinacion.name ORDER BY nombre_combinacion.idAttribute SEPARATOR \' - \') AS combination_name,
-        combinacion.reference as reference_combination,
-        combinacion.ean13 as ean13_combination,
-        ROUND(product.price, 2) AS price,
+        combinacion.reference AS reference_combination,
+        combinacion.ean13 AS ean13_combination,
+        ROUND(product.price * 1.21, 2) AS price_incl_tax,
         stock.quantity,
         tienda.name AS shop_name,
         CONCAT(\'https://mayret.com/img/p/\', 
@@ -53,28 +55,54 @@ class ProductController extends AbstractController
            SUBSTRING(image.id_image, 3, 1), \'/\', 
            SUBSTRING(image.id_image, 4, 1), \'/\', 
            SUBSTRING(image.id_image, 5, 1), \'/\', 
-           image.id_image, \'.jpg\') AS image_url
+           image.id_image, \'.jpg\') AS image_url,
+        CASE 
+          WHEN specific_price.id_specific_price IS NOT NULL THEN 
+            CASE 
+              WHEN specific_price.price >= 0 THEN ROUND(specific_price.price * 1.21, 2)
+              ELSE 
+                CASE 
+                  WHEN specific_price.reductionType = \'amount\' THEN ROUND((product.price - specific_price.reduction) * 1.21, 2)
+                  WHEN specific_price.reductionType = \'percentage\' THEN ROUND((product.price * (1 - specific_price.reduction)) * 1.21, 2)
+                  ELSE ROUND(product.price * 1.21, 2)
+                END
+            END
+        ELSE 
+          ROUND(product.price * 1.21, 2)
+        END AS final_price_incl_tax
     ')
-            ->from(PsStockAvailable::class, 'stock')
-            ->innerJoin('stock.id_product', 'product')
-            ->innerJoin(PsProductLang::class, 'nombre_producto', 'WITH', 'product.id_product = nombre_producto.id_product')
-            ->innerJoin(PsProductAttribute::class, 'combinacion', 'WITH', 'stock.id_product_attribute = combinacion.id_product_attribute')
-            ->innerJoin(PsProductAttributeCombination::class, 'combi', 'WITH', 'combinacion.id_product_attribute = combi.id_product_attribute')
-            ->innerJoin(PsAttributeLang::class, 'nombre_combinacion', 'WITH', 'combi.idAttribute = nombre_combinacion.idAttribute')
-            ->leftJoin(PsShop::class, 'tienda', 'WITH', 'stock.id_shop = tienda.id_shop')
-            ->leftJoin(PsProductAttributeImage::class, 'product_image', 'WITH', 'combinacion.id_product_attribute = product_image.id_product_attribute')
-            ->leftJoin(PsImage::class, 'image', 'WITH', 'product_image.id_image = image.id_image')
-            ->groupBy('product.id_product, combinacion.id_product_attribute, tienda.id_shop')
-            ->orderBy('product.id_product', 'DESC')
-            ->addOrderBy('combinacion.id_product_attribute')
-            ->addOrderBy('tienda.id_shop', 'DESC')
-            ->andWhere('combinacion.reference LIKE :searchTerm OR combinacion.ean13 LIKE :searchTerm')
-            ->setParameter('searchTerm', '%' . $b . '%');
+    ->from(PsStockAvailable::class, 'stock')
+    ->innerJoin('stock.id_product', 'product')
+    ->innerJoin(PsProductLang::class, 'nombre_producto', 'WITH', 'product.id_product = nombre_producto.id_product AND nombre_producto.id_lang = 1')
+    ->innerJoin(PsProductAttribute::class, 'combinacion', 'WITH', 'stock.id_product_attribute = combinacion.id_product_attribute')
+    ->innerJoin(PsProductAttributeCombination::class, 'combi', 'WITH', 'combinacion.id_product_attribute = combi.id_product_attribute')
+    ->innerJoin(PsAttributeLang::class, 'nombre_combinacion', 'WITH', 'combi.idAttribute = nombre_combinacion.idAttribute')
+    ->innerJoin(PsProductShop::class, 'product_shop', 'WITH', 'stock.id_product = product_shop.id_product')
+    ->innerJoin(PsShop::class, 'tienda', 'WITH', 'stock.id_shop = tienda.id_shop')
+    ->leftJoin(PsProductAttributeImage::class, 'product_image', 'WITH', 'combinacion.id_product_attribute = product_image.id_product_attribute')
+    ->leftJoin(PsImage::class, 'image', 'WITH', 'product_image.id_image = image.id_image')
+    ->leftJoin(PsSpecificPrice::class, 'specific_price', 'WITH', '
+        specific_price.id_product = product.id_product 
+        AND (specific_price.id_product_attribute = 0 OR specific_price.id_product_attribute = combinacion.id_product_attribute)
+        AND specific_price.id_customer = 0
+        AND (specific_price.id_group = 7 OR specific_price.id_group = 0)
+        AND specific_price.fromQuantity <= 1
+    ')
+    ->groupBy('product.id_product, combinacion.id_product_attribute, tienda.id_shop')
+    ->orderBy('product.id_product', 'DESC')
+    ->addOrderBy('combinacion.id_product_attribute')
+    ->addOrderBy('tienda.id_shop', 'DESC')
+    ->andWhere('combinacion.reference LIKE :searchTerm OR combinacion.ean13 = :searchTerm2')
+    ->setParameter('searchTerm', '%' . $b . '%')
+    ->setParameter('searchTerm2', $b);
+    
 
         $resultado = $qb->getQuery()->getResult();
 
         foreach ($resultado as &$row) {
-            $row['price'] = (float) $row['price'];
+            $row['price_incl_tax'] = (float) $row['price_incl_tax'];
+            $row['final_price_incl_tax'] = (float) $row['final_price_incl_tax'];
+
         }
 
         return new JsonResponse($resultado);
