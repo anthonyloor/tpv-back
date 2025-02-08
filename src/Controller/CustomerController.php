8c9382129256f02f9fb6676bae9bf96bc36130be
BKLP22 <97519892+BKLP22@users.noreply.github.com>
2025-02-08 16:54:31 +0100

    Fix namespace import for PsCustomerMaylu in CustomerController

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\PsCustomer;
-use App\EntityFaJasMaylu\PsCustomer as PsCustomerMaylu;
+use App\EntityFajasMaylu\PsCustomer as PsCustomerMaylu;
 use App\Entity\PsAddress;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
