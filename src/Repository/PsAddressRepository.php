b6f133fe5a37e420d919abaf3f63557747eda197
BKLP22 <97519892+BKLP22@users.noreply.github.com>
2025-03-18 20:07:34 +0100

    Refactor PsAddressRepository and PsAddressFajasMayluRepository to use EntityManager for improved database interaction

--- src/Repository/PsAddressRepository.php
+++ src/Repository/PsAddressRepository.php
@@ -4,17 +4,23 @@ namespace App\Repository;
 use App\Entity\PsAddress;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PsAddressRepository extends ServiceEntityRepository
 {
+    private EntityManagerInterface $em;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, PsAddress::class);
+        $this->em = $registry->getManager('default');
     }
 
     public function findAllByCustomerId(int $idCustomer): array
     {
-        return $this->createQueryBuilder('a')
+        return $this->em->createQueryBuilder()
+            ->select('a')
+            ->from(PsAddress::class, 'a')
             ->andWhere('a.id_customer = :idCustomer')
             ->setParameter('idCustomer', $idCustomer)
             ->getQuery()
--- src/RepositoryFajasMaylu/PsAddressFajasMayluRepository.php
+++ src/RepositoryFajasMaylu/PsAddressFajasMayluRepository.php
@@ -4,17 +4,24 @@ namespace App\RepositoryFajasMaylu;
 use App\EntityFajasMaylu\PsAddress;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PsAddressFajasMayluRepository extends ServiceEntityRepository
 {
+    private EntityManagerInterface $em;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, PsAddress::class);
+        $this->em = $registry->getManager('fajas_maylu');
+
     }
 
     public function findAllByCustomerId1(int $idCustomer): array
     {
-        return $this->createQueryBuilder('a')
+        return $this->em->createQueryBuilder()
+            ->select('a')
+            ->from(PsAddress::class, 'a')
             ->andWhere('a.id_customer = :idCustomer')
             ->setParameter('idCustomer', $idCustomer)
             ->getQuery()
