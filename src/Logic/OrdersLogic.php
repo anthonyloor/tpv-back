e4a09a12ef63b2a5a80b40d6a937528af74f12dc
Anthony Loor <111461610+anthonyloor@users.noreply.github.com>
2025-03-25 15:44:51 +0100

    Update OrdersLogic.php

--- src/Logic/OrdersLogic.php
+++ src/Logic/OrdersLogic.php
@@ -60,6 +60,7 @@ class OrdersLogic
         $newPsOrder->setTotalPaidTaxExcl($data['total_paid_tax_excl']);
         $newPsOrder->setTotalPaidReal($data['total_paid']);
         $newPsOrder->setTotalProducts($data['total_products']);
+        $newPsOrder->setTotalShipping(0);
         $newPsOrder->setTotalProductsWt($data['total_paid']);
         $newPsOrder->setInvoiceNumber(0);
         $newPsOrder->setInvoiceDate(new \DateTime('now', new \DateTimeZone('Europe/Berlin')));
