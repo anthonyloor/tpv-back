dd82f2dd968ffcbc80dc43d393e49c49986db36a
BKLP22 <97519892+BKLP22@users.noreply.github.com>
2025-04-04 15:38:28 +0200

    Refactor: Remove deprecated REST services and utility classes
    - Deleted RESTDCCConfirmationService, RESTInitialRequestService, RESTOperationService, and RESTService classes to streamline the API.
    - Removed utility classes RESTAnnotation, RESTLogger, and RESTSignatureUtils to eliminate unused logging and signature handling functionalities.
    - Cleaned up initRedsysApi.php and redsysLibrary.php by removing legacy validation functions and initialization code.
    - Ensured that all related files and dependencies are removed to maintain a clean codebase.

--- src/Controller/RedsysPaymetController.php
+++ src/Controller/RedsysPaymetController.php
@@ -2,41 +2,21 @@
 
 namespace App\Controller;
 
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
-use App\Redsys\RedsysService; 
+use Symfony\Component\Routing\Attribute\Route;
 
 class RedsysPaymetController extends AbstractController
 {
-    private $redsysService;
 
-    public function __construct(RedsysService $redsysService)
-    {
-        // Inyecta el servicio RedsysService en el controlador
-        $this->redsysService = $redsysService;
-    }
 
     #[Route('/payment', name: 'payment', methods: ['POST'])]
-    public function createPayment(Request $request)
+    public function createPayment(Request $request): JsonResponse
     {
-        $params = json_decode($request->getContent(), true);
 
-        if (!isset($params['amount']) || !isset($params['order'])) {
-            return new JsonResponse(['error' => 'Faltan parámetros necesarios'], 400);
-        }
 
-        $response = $this->redsysService->createPaymentRequest($params['amount'], $params['order']);
-        dump($response); // Para depuración, puedes eliminarlo después
-        if ($response->getResult() == 'KO') {
-            return new JsonResponse(['result' => $response->getResult()], 500);
-        }
-
-        return new JsonResponse([
-            'signature' => $response['signature'],  
-            'url' => "https://sis-t.redsys.es:25443/sis/realizarPago", 
-            'params' => $response['params'] 
-        ]);
     }
 }
