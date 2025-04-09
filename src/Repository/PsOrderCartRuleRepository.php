6c2c087ae3ec7af8a3788258b933fc1409ab2bf5
BKLP22 <97519892+BKLP22@users.noreply.github.com>
2025-04-09 20:39:43 +0200

    Refactor PsOrderCartRuleRepository to use specific EntityManager for FajasMaylu

--- src/Repository/PsOrderCartRuleRepository.php
+++ src/Repository/PsOrderCartRuleRepository.php
@@ -5,14 +5,20 @@ use App\Entity\PsCartRule;
 use App\Entity\PsOrderCartRule;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Utils\Constants\DatabaseManagers;
 
 
 class PsOrderCartRuleRepository extends ServiceEntityRepository
 {
+    private EntityManagerInterface $em;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, PsOrderCartRule::class);
+        $this->em = $registry->getManager(DatabaseManagers::MAYRET_MANAGER);
     }
+
 
     public function findByIdOrder(int $id)
     {
--- src/RepositoryFajasMaylu/PsOrderCartRuleRepository.php
+++ src/RepositoryFajasMaylu/PsOrderCartRuleRepository.php
@@ -4,13 +4,18 @@ namespace App\RepositoryFajasMaylu;
 use App\EntityFajasMaylu\PsOrderCartRule;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Utils\Constants\DatabaseManagers;
 
 
 class PsOrderCartRuleRepository extends ServiceEntityRepository
 {
+    private EntityManagerInterface $em;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, PsOrderCartRule::class);
+        $this->em = $registry->getManager(DatabaseManagers::FAJASMAYLU_MANAGER);
     }
 
     public function findByIdOrder(int $id)
